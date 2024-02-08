5e837db8a71feaa04d83eee5dbc7bdbc92929822
giselagm37 <119113882+giselagm37@users.noreply.github.com>
2024-02-08 09:39:13 -0300

    mejoras

--- HTML/TrabajoPractico1/JavaScript/TrabajoPracticoAulico/Codigo.js
+++ HTML/TrabajoPractico1/JavaScript/TrabajoPracticoAulico/Codigo.js
@@ -1,4 +1,36 @@
+//EJERCICIO 1//
 
+promedio = 0;
+for (let i = 1; i <= 3; i++) {
+    let numero = parseFloat(prompt("Ingrese el numero " + i + " : "));
+    suma += numero;
+}
+promedio = suma / 3;
+console.log(promedio);
+
+//EJERCICIO 2//
+
+let text = prompt('Ingrese un texto')
+let largo = text.length;
+let mayuscula = text.toUpperCase();
+console.log('La Cantidad de mayusculas del texto es : ' + largo);
+console.log('El texto en mayusculas se ve asi : ' + mayuscula);
+
+//EJERCICIO 3//
+
+let num1 = parseFloat(prompt("Ingrese un numero"));
+let num2 = parseFloat(prompt("Ingrese un numero"));
+let num3 = parseFloat(prompt("Ingrese un numero"));
+if (num1 > num2 && num1 > num3) {
+    alert("Este numero es el mas grande " + num1);
+} else if (num2 > num3 && num2 > num1) {
+    alert("Este numero es el mas grande" + num2);
+} else if (num3 > num2 && num3 > num1) {
+    alert("Este numero es el mas grande" + num3);
+
+} else {
+    alert("Estos numeros son iguales");
+}
 
 
 
@@ -33,14 +65,14 @@ console.log("La suma de los numeros ingresados es : " + suma)
 
 //EJERCICIO 6//
 
-let arr =[32,45,78,99,12,21];
-let= nuevo=[];
+let arr = [32, 45, 78, 99, 12, 21];
+let = nuevo = [];
 
-for(let i=0;i<arr.length;i++){
-    if(arr[i]%3==0){
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 3 == 0) {
         nuevo.push(arr[i]);
     }
 }
-console.log('Arreglo nuevo con los multiplos de 3: '+nuevo);
+console.log('Arreglo nuevo con los multiplos de 3: ' + nuevo);
 
 
